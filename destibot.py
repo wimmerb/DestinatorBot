913d26c4234fa01cb3bdd807d6e4a5c5eb2d942e
Benedikt Wimmer <expert239@Benes-MacBook-Pro.local>
2020-02-11 22:49:21 +0100

    Added keyboard markup

--- destibot.py
+++ destibot.py
@@ -6,6 +6,37 @@ import time
 import re
 from itertools import chain
 
+
+
+
+#1 Wort -> Query mode
+#/again, 2tes go
+#do_category
+#sticker bei confused/beim auswählen
+#user kann query saven
+#number ranges
+#pattern über ganzen string matchen
+#mehr Kategorien, größere Auswahl innerhalb
+#feedback
+#resize
+
+class Reply_Keyboard_Entity:
+    def __init__(self, x):
+        self.val = {'keyboard': x}
+    def give_command(self):
+        return f"&reply_markup={json.dumps(self.val)}"
+class Reply_Keyboard_None(Reply_Keyboard_Entity):
+    def __init__(self):
+        self.val = None
+    def give_command(self):
+        remove_keyboard = {"remove_keyboard": True}
+        return f"&reply_markup={json.dumps(remove_keyboard)}"
+class Reply_Keyboard_Ignore(Reply_Keyboard_Entity):
+    def __init__(self):
+        self.val = None
+    def give_command(self):
+        return f""
+
 latest_update_served = 0
 with open("eastereggs.json") as f:
     modes = json.load(f)
@@ -30,37 +61,52 @@ def extract_choices(msg):
     return l
 
 
-three_dices = [u'🎲', u'🎲🎲', u'🎲🎲🎲']
+three_dices = [u'🎲', u'🎲🎲', u'🎲🎲🎲', "Bow to your destiny!"]
 
-confused = "I'm not sure what you are saying 🤔 Can you use some /help?"
+confused = Reply_Keyboard_Entity([["👍", "👎"]]), ["I'm not sure what you are saying 🤔 Can you use some /help?"]
+helpkeyboard = Reply_Keyboard_Entity([["/help", "👎"]])
 
+
+def do_go(state, message, info):
+    expecting_go = state.get('expecting_go', False)
+    expecting_help = state.get('expecting_help', False)
+    if expecting_go and state['choices'] != []:
+        choice = choose(state['choices'])
+        state['expecting_go'] = False
+        return Reply_Keyboard_None(), three_dices + [choice]
+    else:
+        state['expecting_help'] = True
+        state['expecting_go'] = False
+        return confused
+
+def do_abort(state, message, info):
+    state['expecting_help'] = False
+    state['expecting_go'] = False
+    return Reply_Keyboard_None(), ["Ok, let's try again!"]
 
 def do_yes(state, message, info):
     expecting_go = state.get('expecting_go', False)
     expecting_help = state.get('expecting_help', False)
-    if expecting_go:
-        choice = choose(state['choices'])
-        state['expecting_go'] = False
-        return three_dices + [choice]
-    elif expecting_help:
-        state['expecting_help'] = False
-        return ["Ok, sure! Try this to press this: /help"]
+    if expecting_help:
+        state['expecting_help'] = True
+        return helpkeyboard, ["Ok, sure! Try to press this: /help"]
     else:
         state['expecting_help'] = True
-        return [confused]
+        state['expecting_go'] = False
+        return confused
 
 
 def do_no(state, message, info):
     expecting_go = state.get('expecting_go', False)
     expecting_help = state.get('expecting_help', False)
-    if expecting_go:
-        return ["Ok! You can add some more choices:"]
-    elif expecting_help:
+    if expecting_help:
         state['expecting_help'] = False
-        return ["Ok, sure!"]
+        return Reply_Keyboard_None(), ["Ok, sure!"]
     else:
         state['expecting_help'] = True
-        return [confused]
+        state['expecting_go'] = False
+        return confused
+
 
 
 def do_start(state, message, info):
@@ -68,31 +114,38 @@ def do_start(state, message, info):
 This is your destinator!
 Can I help you find your destiny today?
 If you are unsure what to do, you can try /help"""
-    return [welcome]
+    return Reply_Keyboard_None(), [welcome]
 
 
 def do_help(state, message, info):
     state['expecting_help'] = False
+    state['expecting_go'] = False
     suggestions = info['suggestions']
     welcome = info['welcome']
-    return [welcome] + suggestions
+    return Reply_Keyboard_None() , [welcome] + suggestions
 
 
 def do_query(state, message, info):
     state['expecting_go'] = True
-    state['choices'] = list(info['choices'])
+    state['expecting_help'] = False
+    state['choices'] = []
     query = info.get('query', "Ready to find your destiny?")
-    choice_text = info.get('choice_text', "These are your choices:")
+    choice_text = info.get('choice_text', "Give me some choices:")
     state['query'] = query
     state['choice_text'] = choice_text
-    return [choice_text] + state['choices'] + [query]
+    return Reply_Keyboard_Entity([list(info['choices']),['🎲', '❌']]), [choice_text] + state['choices']# + [query]
 
 
 def do_persons(state, message, info):
+    #do default
+    return do_default(state, message)
+    #hidden code
+    """
     items = extract_choices(message)
     patterns = info['patterns']
     choices = info['choices']
 
+    
     def replace(choice):
         for pattern in patterns:
             if re.match(pattern, choice):
@@ -100,7 +153,7 @@ def do_persons(state, message, info):
         return [choice]
     choices = list(chain(*map(replace, items)))
     choice = choose(choices)
-    return three_dices + [choice]
+    return three_dices + [choice]"""
 
 
 def do_default(state, message):
@@ -108,9 +161,9 @@ def do_default(state, message):
     expecting_go = state.get('expecting_go', False)
     if expecting_go:
         state['choices'] += items
-        return [state['choice_text']] + state['choices'] + [state['query']]
+        return Reply_Keyboard_Ignore(), []
     choice = choose(items)
-    return three_dices + [choice]
+    return Reply_Keyboard_None(), three_dices + [choice]
 
 
 modes_to_functions = {
@@ -119,24 +172,33 @@ modes_to_functions = {
     "start": do_start,
     "help": do_help,
     "persons": do_persons,
-    "query": do_query
+    "query": do_query,
+    "go": do_go,
+    "abort": do_abort
 }
 
 
 def process_message(message, chatid):
     global states
+    #found "bug": wenn man einfach nur einmal ohne Kontext "jo" eingibt -> er landet bei do_yes
     if chatid not in states:
         states[chatid] = {}
     state = states[chatid]
     for name, info in modes.items():
         patterns = info["patterns"]
-        ignore_case = info.get("ignore_case", True)
+        ignore_case = info.get("ignore_case", True) #findet noch keine Verwendung
         flags = 0
         if ignore_case:
+            #(mehrere) Flags setzen mit bitwise or
             flags |= re.IGNORECASE
         for pattern in patterns:
+            #checke alle Patterns nach match
             if re.match(pattern, message, flags):
+                #handler für gematchtes Pattern raussuchen
                 handler = modes_to_functions[info['mode']]
+                #state: gespeichert zu chatid, enthält 'expecting_go', 'expecting_help', 'choices'
+                #message: gesendeter Text
+                #info: patterns, mode, choices
                 response = handler(state, message, info)
                 return response
     response = do_default(state, message)
@@ -153,19 +215,20 @@ def handle_update(update, basic_bot_url):
         # Not sure what to do here
         return
     chatid = update["message"]["from"]["id"]
+    #möglich: mehr als nur Textnachrichten handlen z.B. Kette von Sticker-Nachrichten als Eingabe
     if "text" not in update["message"]:
-        responses = [confused]
+        keyboard, responses = confused
     else:
         text = update["message"]["text"]
-        # failt bei leerer Liste
-        responses = process_message(text, chatid)
-    # fstring refactor
-    for response in responses[:-1]:
+        keyboard, responses = process_message(text, chatid)
+    for i in range(len(responses)):
+        reply_keyboard_markup = keyboard.give_command()
+        response = responses[i]
         requests.get(
-            f"{basic_bot_url}/{send_message}?chat_id={chatid}&text={response}")
-        time.sleep(0.3)
-    requests.get(
-        f"{basic_bot_url}/{send_message}?chat_id={chatid}&text={responses[-1]}")
+                     f"{basic_bot_url}/{send_message}?chat_id={chatid}&text={response}{reply_keyboard_markup}")
+        print(f"{basic_bot_url}/{send_message}?chat_id={chatid}&text={response}{reply_keyboard_markup}")
+        if i < (len(responses) - 1):
+            time.sleep(0.3)
 
 
 def task():
@@ -175,8 +238,12 @@ def task():
     get_updates = "getUpdates"
     basic_bot_url = f"{telegram_api_url}/bot{bot_token}"
     update_request_url = f"{basic_bot_url}/{get_updates}?offset={latest_update_served+1}"
+    print(update_request_url)
     req = requests.get(update_request_url)
-    res = req.json()["result"]
+    res = req.json()
+    if res["ok"] != True:
+        return
+    res = res["result"]
     for update in res:
         update_id = update["update_id"]
         if update_id > latest_update_served:
@@ -188,3 +255,9 @@ if __name__ == "__main__":
     while True:
         task()
         time.sleep(0.3)
+
+
+
+
+
+
